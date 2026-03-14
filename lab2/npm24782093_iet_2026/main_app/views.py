087cb1a0141b9ea81d4543a7a9d296a5109f25f4
riyan <riyanrahmadan75@gmail.com>
2026-03-14 22:14:57 +0700

    membenarkan struktur folder lab2

---
+++ lab2/npm24782093_iet_2026/main_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'main_app/home.html')
+
+
+
+# from django.shortcuts import render
+
+# Create your views here.
